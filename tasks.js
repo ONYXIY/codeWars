2f1a1dd39b3d8dbc76f89289bbf20fdff3cf86d5
ONYXIY <onxvnezakona@gmail.com>
2023-09-15 12:49:39 +0300

    tasks

--- tasks.js
+++ tasks.js
@@ -7,8 +7,60 @@
 // [9, 8, 7, 6, 5, 4, 3, 2, 1, 0]  =>  [1, 8, 3, 6, 5, 4, 7, 2, 9, 0]
 
 function sortArray(array) {
-  const newArr = [...array].filter(item => item % 2 !== 0).sort((a, b) => a - b);
-  const res = array.map( item => (item % 2 !== 0 ? newArr.shift(): item ))
-  return res
+  const newArr = [...array]
+    .filter((item) => item % 2 !== 0)
+    .sort((a, b) => a - b);
+  const res = array.map((item) => (item % 2 !== 0 ? newArr.shift() : item));
+  return res;
 }
+//---------------------------------------------------------------------------------
+//how many each element occurs in the array?
+//{kiwi: 3, apple: 2, orange: 1}
+const fruits = ["kiwi", "apple", "kiwi", "orange", "kiwi", "apple"];
+
+const fruitCount = (arr) => {
+  const count = {};
+  arr.forEach((item) => {
+    if (!count[item]) {
+      count[item] = 1;
+    } else {
+      count[item]++;
+    }
+  });
+  return count;
+};
+
+//---------------------------------------------------------------------------------
+//Create an array with unique values
+const myFruits = ["kiwi", "apple", "kiwi", "orange", "kiwi", "apple"];
+
+const createUniqArr = (arr) => {
+  let UniqArr = [...new Set(arr)];
+  return UniqArr;
+};
+
+//---------------------------------------------------------------------------------
+// Создать функцию, которая сгруппирует студентов по возрасту
+// На выходе получить объект, где ключ - возраст. а значение массив студентов
+
+const students = [
+  { name: "alex", age: 20 },
+  { name: "mike", age: 24 },
+  { name: "masha", age: 20 },
+  { name: "sasha", age: 18 },
+];
+
+const ageGroup = (arr) => {
+  let group = {};
+  arr.forEach((item) => {
+    if (!group[item.age]) {
+      group[item.age] = [item];
+    } else {
+      group[item.age].push(item);
+    }
+  });
+  return group;
+};
+console.log(ageGroup(students));
+
 //---------------------------------------------------------------------------------
